c600a0589bf0ee78d81fb1038e95af81f3122d6f
Scare01 <lexsl84@gmail.com>
2019-01-29 18:33:45 +0700

    add actions

---
+++ src/redux/actions/index.js
@@ -0,0 +1,43 @@
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  EDIT_TODO,
+  SET_VISIBILITY_FILTER
+} from "../constants/actionTypes";
+import v4 from "uuid";
+
+export const addTodo = (name, description, importance, dateToComplete) => ({
+  type: ADD_TODO,
+  name,
+  description,
+  importance,
+  dateToComplete,
+  id: v4(),
+  completed: false
+});
+
+export const deleteTodo = id => ({
+  type: DELETE_TODO,
+  id
+});
+
+export const editTodo = (
+  id,
+  name,
+  description,
+  importance,
+  dateToComplete
+) => ({
+  type: EDIT_TODO,
+  id,
+  name,
+  description,
+  importance,
+  dateToComplete,
+  completed: false
+});
+
+export const setVisibilityFilter = filter({
+  type: SET_VISIBILITY_FILTER,
+  filter
+});
