e3970a7ec03d62c379232f9dfc9dd2c0070eb60b
Scare01 <lexsl84@gmail.com>
2019-01-29 18:20:40 +0700

    add constants for filter

---
+++ src/redux/constants/visibilityFilters.js
@@ -0,0 +1,8 @@
+let visibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_NORMAL: "SHOW_NORMAL",
+  SHOW_IMPORTANT: "SHOW_IMPORTANT",
+  SHOW_VERY_IMPORTANT: "SHOW_VERY_IMPORTANT"
+};
+
+export default visibilityFilters;
