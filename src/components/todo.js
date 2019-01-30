9ee09dde0ba1ce6283d5ed41d74df4332eb9aa5e
Scare01 <lexsl84@gmail.com>
2019-01-30 12:37:26 +0700

    change todo to class

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,18 +1,35 @@
 import React from "react";
 
-let Todo = ({ todo, clickEdit, clickDelete, clickCompleted }) => (
-  <div className="todo">
-    <h2>{todo.name}</h2>
-    <h4>{todo.importance}</h4>
-    <p>{todo.description}</p>
-    <p>Completed to {todo.dateToComplete}</p>
-    {todo.completed ? null : <button onClick={clickEdit}>Edit</button>}
-    {todo.completed ? null : (
-      <button onClick={clickCompleted}>Completed</button>
-    )}
-    {todo.completed ? <p>Completed date: {todo.completedDate}</p> : null}
-    <button onClick={clickDelete}>Delete</button>
-  </div>
-);
+class Todo extends React.Component {
+  render() {
+    let EditButton = this.props.todo.completed ? null : (
+      <button onClick={this.props.clickEdit}>Edit</button>
+    );
+    let CompeleteDate = this.props.todo.dateToComplete ? (
+      <p>Completed to {this.props.todo.dateToComplete}</p>
+    ) : null;
+
+    let CompletedButton = this.props.todo.completed ? null : (
+      <button onClick={this.props.clickCompleted}>Completed</button>
+    );
+
+    let TodoCompeleted = this.props.todo.completed ? (
+      <p>Completed date: {this.props.todo.completedDate}</p>
+    ) : null;
+
+    return (
+      <div className="todo">
+        <h2>{this.props.todo.name}</h2>
+        <h4>{this.props.todo.importance}</h4>
+        <p>{this.props.todo.description}</p>
+        {CompeleteDate}
+        {EditButton}
+        {CompletedButton}
+        {TodoCompeleted}
+        <button onClick={this.props.clickDelete}>Delete</button>
+      </div>
+    );
+  }
+}
 
 export default Todo;
