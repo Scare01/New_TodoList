3f1fbb70b6b9471b1a57ef0fc1432a167c3b1f40
Scare01 <lexsl84@gmail.com>
2019-01-30 11:41:01 +0700

    todoList

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,8 +1,45 @@
-import React from 'react'
-import Todo from './todo'
+import React from "react";
+import Todo from "./todo";
+import { deleteTodo, editTodo, toggleTodo } from "../redux/actions/index";
+import { connect } from "react-redux";
 
-let TodoList = ({ todos }) => (
-  {todos.map(todo => {
-    <Todo todo={todo} />
-  })}
-)
+let mapStateToProps = state => {
+  return { todos: state.todos };
+};
+
+let mapDispatchToProps = dispatch => {
+  return {
+    clickCompleted: id => dispatch(toggleTodo(id)),
+    clickDelete: id => dispatch(deleteTodo(id)),
+    clickEdit: (id, name, description, importance, dateToComplete) =>
+      dispatch(editTodo(id, name, description, importance, dateToComplete))
+  };
+};
+
+let List = ({ todos, clickCompleted, clickDelete, clickEdit }) => (
+  <div>
+    {todos.map(todo => {
+      <Todo
+        todo={todo}
+        clickCompleted={() => clickCompleted(todo.id)}
+        clickDelete={() => clickDelete(todo.id)}
+        clickEdit={() =>
+          clickEdit(
+            todo.id,
+            todo.name,
+            todo.description,
+            todo.importance,
+            todo.dateToComplete
+          )
+        }
+      />;
+    })}
+  </div>
+);
+
+let TodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(List);
+
+export default TodoList;
